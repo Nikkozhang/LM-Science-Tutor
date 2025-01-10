10675741326c7865f2e132b157b349e33eb41703
nikkozhang <nikkozhang723@gmail.com>
2025-01-09 22:17:56 -0600

    add test

--- train/test.sh
+++ train/test.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 
-#SBATCH --job-name=finetune
-#SBATCH --partition=mm-gpu
-#SBATCH --ntasks=8        # Total number of tasks
-#SBATCH --nodes=1         # Request 1 node
-#SBATCH --gpus=8          # Request 8 GPUs in total
-#SBATCH --cpus-per-task=1 # Allocate CPU cores per task if needed
-#SBATCH --output=slurm_%A.out  # Output file
-#SBATCH --error=slurm_%A.err   # Error file
-#SBATCH --open-mode=append
-#SBATCH --export=ALL,IS_REMOTE=1
+#SBATCH --job-name=test
 
+#SBATCH --partition=gpu
+#SBATCH -G 1
+
+#SBATCH --output=slurm.out
+#SBATCH --error=slurm.err
+#SBATCH --open-mode=append
+
+#SBATCH --export=ALL,IS_REMOTE=1
 export HF_HOME=/share/data/mei-work/nikkozhang/LM-Science-Tutor/huggingface_cache
 export HUGGINGFACE_TOKEN="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"
 
